f832798a1aa90b8ebe9d0e05ce309a4550fd9c7d
Samuel E. Giddins <segiddins@segiddins.me>
2015-08-07 00:45:56 -0700

    Use actual models in hooks controller

--- app/controllers/github_web_hook_controller.rb
+++ app/controllers/github_web_hook_controller.rb
@@ -31,43 +31,43 @@ class GithubWebHookController < ApplicationController
     pull_request = user_client.pull_request(repo, $1)
     parent = $2
     comment = $3
-    combined_status = user_client.combined_status(repo, payload.commit.sha)
-    parent_patronus_status = user_client.statuses(repo, parent).find { |s| s.content = STATUS_CONTEXT }
+    combined_status = user_client.combined_status(repo_name, payload.commit.sha)
+    parent_patronus_status = user_client.statuses(repo_name, parent).find { |s| s.content = STATUS_CONTEXT }
     case combined_status.state
     when "success", "failure"
       unless parent_patronus_status.state == "failure"
-        user_client.create_status(repo, parent, combined_status.state, context: STATUS_CONTEXT)
+        user_client.create_status(repo_name, parent, combined_status.state, context: STATUS_CONTEXT)
       end
-      if combined_status.state == "success" && user_client.combined_status(repo, parent).state == "success" && comment != 'test'
-        user_client.update_branch(repo, pull_request.base.ref, payload.commit.sha, false)
+      if combined_status.state == "success" && user_client.combined_status(repo_name, parent).state == "success" && comment != 'test'
+        user_client.update_branch(repo_name, pull_request.base.ref, payload.commit.sha, false)
       end
-      user_client.delete_branch(repo, "patronus/#{parent}")
+      user_client.delete_branch(repo_name, "patronus/#{parent}")
     else
       # wait until all done
     end
   end
 
   def handle_issue_comment
-    return unless commenter = User.find_by(username: payload.comment.user.login)
-    return unless user_client.collaborator?(repo, commenter.username)
+    commenter = payload.comment.user.login
+    return unless user_client.collaborator?(repo_name, commenter)
     return unless comment = /\Apatronus: /
     issue_number = payload.issue.number
-    return unless pull_request = user_client.pull_request(repo, issue_number)
+    return unless pull_request = user_client.pull_request(repo_name, issue_number)
     head = pull_request.head.sha
     test_branch = "patronus/#{head}"
 
     comment = comment[10..-1]
     case comment
     when ":+1:", "test", "retry"
-      user_client.create_status(repo, head, "pending", context: STATUS_CONTEXT)
-      user_client.create_branch(repo, "heads/#{test_branch}", pull_request.base.sha)
+      user_client.create_status(repo_name, head, "pending", context: STATUS_CONTEXT)
+      user_client.create_branch(repo_name, "heads/#{test_branch}", pull_request.base.sha)
       message = <<-MSG.strip_heredoc
         Auto merge of PR ##{issue_number} by patronus from #{head} onto #{pull_request.base.label}
-        #{commenter.username} => #{comment}
+        #{commenter} => #{comment}
       MSG
-      user_client.merge(repo, pull_request.base.ref, head, test_branch, commit_message: message)
+      user_client.merge(repo_name, pull_request.base.ref, head, test_branch, commit_message: message)
     when ":-1:"
-      user_client.create_status(repo, head, "failure", context: STATUS_CONTEXT)
+      user_client.create_status(repo_name, head, "failure", context: STATUS_CONTEXT)
     end
   end
 
@@ -81,12 +81,12 @@ class GithubWebHookController < ApplicationController
     status = 401 && render(text: "Signatures didn't match!") unless Rack::Utils.secure_compare(signature, expected_signature)
   end
 
-  attr_reader :user, :project, :user_client, :payload, :repo
+  attr_reader :user, :repo_name, :user_client, :payload, :repo
   def repo=(repo)
-    @repo = repo
-    @project = Project.find_by!(repo: repo)
+    @repo_name = repo
+    @repo = Repo.find_by!(name: repo)
     @user = @project.user
-    @user_client = Octokit::Client.new(:access_token => @user.github_user.access_token)
+    @user_client = Octokit::Client.new(:access_token => @user.github_token)
   end
 
   def app_client
