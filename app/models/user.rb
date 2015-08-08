1456a4f401ef33d366fd913fc4d5d9fbd1111ca6
Andre Arko <andre@arko.net>
2015-08-07 23:10:36 -0700

    search by username only, but create with token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_many :repos
 
   def self.create_or_update_from_github!(github_user)
-    user = User.find_or_create_by!(username: github_user.login, github_token: github_user.token)
+    with_token = User.create_with(github_token: github_user.token)
+    user = with_token.find_or_create_by!(username: github_user.login)
     user.update!(github_token: github_user.token)
   end
 
