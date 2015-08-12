5f4bf27098052f4b91e451e42ca108142bf4cc57
Andre Arko <andre@arko.net>
2015-08-11 19:42:54 -0700

    handle users deleted from the DB but logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+private
+
+  def current_user
+    user = User.where(username: github_user.login).first
+    user ||= User.create_or_update_from_github!(github_user)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   def self.create_or_update_from_github!(github_user)
     with_token = User.create_with(github_token: github_user.token)
     user = with_token.find_or_create_by!(username: github_user.login)
-    user.update!(github_token: github_user.token)
+    user.tap{|u| u.update!(github_token: github_user.token) }
   end
 
   def github
