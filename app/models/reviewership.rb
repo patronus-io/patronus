07f450f88b5924f72c75add56682ea328f8a9a07
Samuel E. Giddins <segiddins@segiddins.me>
2015-08-13 12:09:59 -0700

    [Reviewership] Fix getting hook url

--- app/models/reviewership.rb
+++ app/models/reviewership.rb
@@ -5,7 +5,7 @@ class Reviewership < ActiveRecord::Base
   accepts_nested_attributes_for :repo
 
   def ensure_webhook_installed!
-    hook_url = url_for controller: "github_web_hook", action: "create"
+    hook_url = Rails.application.routes.url_helpers.url_for controller: "github_web_hook", action: "create"
     unless user.github.hooks(repo.name).find { |hook| hook.url == hook_url }
       user.github.create_hook(repo.name, "web", { url: hook_url, content_type: :json}, { events: "*", secret: ENV['GITHUB_WEBHOOK_SECRET'.freeze] })
     end
