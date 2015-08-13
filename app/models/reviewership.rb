1b134b009f5d5c0c94b966ff9cc21d69d9817189
Samuel E. Giddins <segiddins@segiddins.me>
2015-08-13 00:52:08 -0700

    Ensure hooks are installed for a given reviewership

--- app/models/reviewership.rb
+++ app/models/reviewership.rb
@@ -3,4 +3,11 @@ class Reviewership < ActiveRecord::Base
   belongs_to :repo
 
   accepts_nested_attributes_for :repo
+
+  def ensure_webhook_installed!
+    hook_url = url_for controller: "github_web_hook", action: "create"
+    unless user.github.hooks(repo.name).find { |hook| hook.url == hook_url }
+      user.github.create_hook(repo.name, "web", { url: hook_url, content_type: :json}, { events: "*", secret: ENV['GITHUB_WEBHOOK_SECRET'.freeze] })
+    end
+  end
 end
