b0fda11a5218ae55545d252eff760645a15726ea
Samuel E. Giddins <segiddins@segiddins.me>
2015-08-13 20:15:08 -0700

    [Reviewership] Ensure the secret is passed when creating hook

--- app/models/reviewership.rb
+++ app/models/reviewership.rb
@@ -7,7 +7,8 @@ class Reviewership < ActiveRecord::Base
   def ensure_webhook_installed!
     hook_url = Rails.application.routes.url_helpers.url_for controller: "github_web_hook", action: "create", host: ENV['GITHUB_WEBHOOK_HOST']
     unless user.github.hooks(repo.name).find { |hook| hook.url == hook_url }
-      user.github.create_hook(repo.name, "web", { url: hook_url, content_type: :json}, { events: "*", secret: ENV['GITHUB_WEBHOOK_SECRET'.freeze] })
+      config = { url: hook_url, content_type: :json, secret: ENV['GITHUB_WEBHOOK_SECRET'.freeze] }
+      user.github.create_hook(repo.name, "web", config, { events: "*" })
     end
   end
 end
