8f2d55bb24a6c6b6a0c7180b638bbca5be267fcb
bronzdoc <lsagastume1990@gmail.com>
2016-05-17 14:21:09 -0600

    Events must be an array

--- app/models/reviewership.rb
+++ app/models/reviewership.rb
@@ -8,7 +8,7 @@ class Reviewership < ActiveRecord::Base
     hook_url = Rails.application.routes.url_helpers.url_for controller: "github_web_hook", action: "create", host: ENV['GITHUB_WEBHOOK_HOST']
     unless user.github.hooks(repo.name).find { |hook| hook.url == hook_url }
       config = { url: hook_url, content_type: :json, secret: ENV['GITHUB_WEBHOOK_SECRET'.freeze] }
-      user.github.create_hook(repo.name, "web", config, { events: "*" })
+      user.github.create_hook(repo.name, "web", config, { events: ["*"] })
     end
   end
 end
