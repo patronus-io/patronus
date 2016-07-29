cdf9ecbd94ebe12278f8a02ac8f9e128b4505fc7
bronzdoc <lsagastume1990@gmail.com>
2016-07-28 18:09:43 -0600

    Handle comment on closed issue

--- app/controllers/github_web_hook_controller.rb
+++ app/controllers/github_web_hook_controller.rb
@@ -58,38 +58,10 @@ class GithubWebHookController < ApplicationController
   end
 
   def handle_issue_comment
-    return unless payload.action.eql? 'created'
-    commenter = payload.comment.user.login
-    return unless bot_client.collaborator?(repo_name, commenter)
-    comment = payload.comment.body
-    return unless comment.gsub!(/\Apatronus: /, "")
-    comment.strip!
-    issue_number = payload.issue.number
-    return unless pull_request = bot_client.pull_request(repo_name, issue_number)
-    head = pull_request.head.sha
-
-    Rails.logger.info { "-> PR #{issue_number} - #{commenter}: #{comment.inspect}, HEAD #{head[0, 7]}" }
-
-    case comment
-    when ":+1:", "test", "retry"
-      test_branch = "patronus/#{head}"
-      Rails.logger.info { "  -> Creating pending status on PR HEAD" }
-      bot_client.create_status(repo_name, head, "pending", context: STATUS_CONTEXT)
-      Rails.logger.info { "  -> Creating test ref #{test_branch.inspect} based on #{pull_request.base.sha[0, 7]}" }
-      target_branch = bot_client.branch(repo_name, pull_request.base.ref)
-      bot_client.create_ref(repo_name, "heads/#{test_branch}", target_branch.commit.sha)
-      message = <<-MSG.strip_heredoc
-        Auto merge of PR ##{issue_number} by patronus from #{head} onto #{pull_request.base.label}
-        #{commenter} => #{comment}
-      MSG
-      Rails.logger.info { "  -> Merging PR HEAD into test branch" }
-      bot_client.merge(repo_name, test_branch, head, commit_message: message)
-    when ":-1:"
-      Rails.logger.info { "  -> Creating failure status on PR HEAD" }
-      bot_client.create_status(repo_name, head, "failure", context: STATUS_CONTEXT)
-    when "fork"
-      Rails.logger.info { "  -> Creating a local branch from PR HEAD" }
-      bot_client.create_ref(repo_name, "heads/patronus-pr-#{issue_number}", head)
+    if payload["issue"]["state"] == "closed"
+      handle_closed_issue
+    else
+      handle_issue
     end
   end
 
@@ -125,6 +97,89 @@ class GithubWebHookController < ApplicationController
   end
 
   private
+
+  def handle_closed_issue
+    return if ENV["CLOSED_ISSUE_DAY_LIMIT"].nil?
+
+    today = DateTime.now
+    Rails.logger.info { "-> closed_at: #{payload["issue"]["closed_at"]}" }
+    Rails.logger.info { "-> closed_at type: #{payload["issue"]["closed_at"].class}" }
+    closed_at = DateTime.parse(payload["issue"]["closed_at"].to_s)
+    # Show warnings only if the issue has been closed for the limit time
+    return if (today - closed_at).to_i < ENV["CLOSED_ISSUE_DAY_LIMIT"].to_i
+
+    commenter = payload["comment"]["user"]["login"]
+    bot = bot_client.user.login
+    # Let the bot and collaborators comment freely
+    return if commenter == bot || bot_client.collaborator?(repo_name, commenter)
+
+    issue_number = payload["issue"]["number"]
+    repo = Repo.find_by(name: repo_name)
+    issue = Issue.find_by(number: issue_number)
+
+    if issue.nil?
+      issue = Issue.new(repo_id: repo.id, number: issue_number, comments_after_closed: 1)
+      issue.save
+    end
+
+    comments = issue.comments_after_closed
+    message = if comments == 2
+                ENV["CLOSED_ISSUE_WARN_MESSAGE_2"]
+              elsif comments > 2
+                # Lock issue this way until octokit support it.
+                # See https://github.com/octokit/octokit.rb/pull/770/files
+                require "http"
+                token = "access_token=#{bot_client.access_token}"
+                uri = "https://api.github.com/repos/#{repo_name}/issues/#{issue_number}/lock?#{token}"
+
+                # https://developer.github.com/v3/issues/#lock-an-issue
+                HTTP.headers("Content-Length" => 0).put(uri)
+
+                "Locking issue."
+              else
+                ENV["CLOSED_ISSUE_WARN_MESSAGE_1"]
+              end
+
+    bot_client.add_comment(repo_name, issue_number, message)
+    issue.comments_after_closed += 1
+    issue.save
+  end
+
+  def handle_issue
+    return unless payload.action.eql? 'created'
+    commenter = payload.comment.user.login
+    return unless bot_client.collaborator?(repo_name, commenter)
+    comment = payload.comment.body
+    return unless comment.gsub!(/\Apatronus: /, "")
+    comment.strip!
+    issue_number = payload.issue.number
+    return unless pull_request = bot_client.pull_request(repo_name, issue_number)
+    head = pull_request.head.sha
+
+    Rails.logger.info { "-> PR #{issue_number} - #{commenter}: #{comment.inspect}, HEAD #{head[0, 7]}" }
+
+    case comment
+    when ":+1:", "test", "retry"
+      test_branch = "patronus/#{head}"
+      Rails.logger.info { "  -> Creating pending status on PR HEAD" }
+      bot_client.create_status(repo_name, head, "pending", context: STATUS_CONTEXT)
+      Rails.logger.info { "  -> Creating test ref #{test_branch.inspect} based on #{pull_request.base.sha[0, 7]}" }
+      target_branch = bot_client.branch(repo_name, pull_request.base.ref)
+      bot_client.create_ref(repo_name, "heads/#{test_branch}", target_branch.commit.sha)
+      message = <<-MSG.strip_heredoc
+        Auto merge of PR ##{issue_number} by patronus from #{head} onto #{pull_request.base.label}
+        #{commenter} => #{comment}
+      MSG
+      Rails.logger.info { "  -> Merging PR HEAD into test branch" }
+      bot_client.merge(repo_name, test_branch, head, commit_message: message)
+    when ":-1:"
+      Rails.logger.info { "  -> Creating failure status on PR HEAD" }
+      bot_client.create_status(repo_name, head, "failure", context: STATUS_CONTEXT)
+    when "fork"
+      Rails.logger.info { "  -> Creating a local branch from PR HEAD" }
+      bot_client.create_ref(repo_name, "heads/patronus-pr-#{issue_number}", head)
+    end
+  end
 
   def verify_signature(payload_body)
     secret = ENV['GITHUB_WEBHOOK_SECRET'.freeze]
